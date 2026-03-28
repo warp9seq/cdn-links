f25e94f675b325fe81e6ba1e1827eb7f317ec049
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2026-03-29 00:23:51 +1100

    perm

--- getcounts.sh
+++ getcounts.sh
@@ -7,6 +7,6 @@ die() {
 }
 
 mkdir logs/ || die "Could not create directory"
-aws s3 cp --no-sign-request --endpoint-url=https://s3.ap-southeast-2.wasabisys.com s3://cdn-bioinf-logs/ logs/ || die "Could not download from wasabi"
+aws s3 cp --no-sign-request --endpoint-url=https://s3.ap-southeast-2.wasabisys.com --recursive s3://cdn-bioinf-logs/ logs/ || die "Could not download from wasabi"
 
 cat logs/* | grep "slorado" | grep "REST.GET.OBJECT" | grep " 200 " | wc -l
