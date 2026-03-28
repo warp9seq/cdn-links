5e64035dbd87d9b6e049001cd357de40436c9428
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2026-03-29 00:18:30 +1100

    Create getcounts.sh

---
+++ getcounts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# terminate script
+die() {
+	echo "$1" >&2
+	exit 1
+}
+
+mkdir logs/ || die "Could not create directory"
+aws s3 cp --no-sign-request --endpoint-url=https://s3.ap-southeast-2.wasabisys.com s3://cdn-bioinf-logs/ logs/ || die "Could not download from wasabi"
+
+cat logs/* | grep "slorado" | grep "REST.GET.OBJECT" | grep " 200 " | wc -l
