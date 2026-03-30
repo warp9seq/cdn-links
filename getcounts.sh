035c2a343c64dd53962c73907a06d90351a4c47e
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2026-03-31 00:40:47 +1100

    try fix

--- getcounts.sh
+++ getcounts.sh
@@ -10,3 +10,11 @@ mkdir logs/ || die "Could not create directory"
 aws s3 cp --no-sign-request --endpoint-url=https://s3.ap-southeast-2.wasabisys.com --recursive s3://cdn-bioinf-logs/ logs/ || die "Could not download from wasabi"
 
 cat logs/* | grep "slorado" | grep "REST.GET.OBJECT" | grep " 200 " | wc -l > slorado.txt
+
+git config --global user.email "action@github.com" || die "Failed to configure git email"
+git config --global user.name "GitHub Action" || die "Failed to configure git name"
+git add slorado.txt || die "adding failed"
+if ! git diff --cached --quiet slorado.txt; then
+    git commit -m "Update download counts" || die "commit failed"
+	git push || die "push failed"
+fi
